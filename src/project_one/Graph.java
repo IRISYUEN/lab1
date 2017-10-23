8dbc0299f046e8b5b62b5d5c66415b64d4b59e66
IRISYUEN <yyf775@sina.cn>
2017-10-23 22:55:12 +0800

    newversion

--- src/project_one/Graph.java
+++ src/project_one/Graph.java
@@ -1,13 +1,16 @@
+package lab1;
 
-package project_one;
 import java.util.List;
-public class Graph{
-    int wordNum;//椤剁偣鏁�
-    List<String> wordList; //椤剁偣闆�
-    int[][] E; //杈归泦
-    public Graph(List<String> wordList,int[][] E,int wordNum){
-        this.wordList = wordList;
-        this.E = E;
-        this.wordNum = wordNum;
-    }   
+//修改记录7
+public class Graph { int wordNum;// 顶点数
+  List<String> wordList; // 顶点集
+  int[][] e; // 边集
+
+  public Graph(final List<String> wordList,final int[][] e, final int wordNum) {
+    this.wordList = wordList;
+    this.e = e;
+    this.wordNum = wordNum;
+   
+  }
+
 }
